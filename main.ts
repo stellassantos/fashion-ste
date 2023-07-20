b173bee47ef5d43cd04587b511dbb08c3ee2b519
Stella Soares Santos <54483907+stellassantos@users.noreply.github.com>
2023-07-20 15:50:18 -0300

    Initialize auto-complete-api

---
+++ main.ts
@@ -0,0 +1,22 @@
+/// <reference no-default-lib="true"/>
+/// <reference lib="dom" />
+/// <reference lib="deno.ns" />
+/// <reference lib="esnext" />
+
+import twindPlugin from "$fresh/plugins/twind.ts";
+import { start } from "$fresh/server.ts";
+import { $live } from "$live/mod.ts";
+import partytownPlugin from "partytown/mod.ts";
+import manifest from "./live.gen.ts";
+import twindConfig from "./twind.config.ts";
+import site from "./site.json" assert { type: "json" };
+
+await start($live(manifest, site), {
+  plugins: [
+    partytownPlugin(),
+    twindPlugin({
+      ...twindConfig,
+      selfURL: new URL("./twind.config.ts", import.meta.url).href,
+    }),
+  ],
+});
