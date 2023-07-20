3ffec74fe732e67aba8865e4e2082171a3889b02
Stella Soares Santos <54483907+stellassantos@users.noreply.github.com>
2023-07-20 15:50:01 -0300

    Initialize luiza/vtex-search-loaders

---
+++ clients/instances.ts
@@ -0,0 +1,21 @@
+import { createClient as createVTEXClient } from "$live/std/commerce/vtex/client.ts";
+import { createClient as createShopifyClient } from "$live/std/commerce/shopify/client.ts";
+import { createClient as createOccClient } from "$live/std/commerce/occ/client.ts";
+
+export const vtex = createVTEXClient({
+  platform: "vtex",
+  account: "lojaoffpremium",
+  defaultLocale: "pt-BR",
+  defaultSalesChannel: "1",
+});
+
+export const shopify = createShopifyClient({
+  platform: "shopify",
+  storeName: "gimenesdevstore",
+  storefrontAccessToken: "27c1ac16fe30a0fb6c5d634eeb63bf81",
+});
+
+export const occ = createOccClient({
+  platform: "occ",
+  baseUrl: "https://deco.cx.teste/",
+});
