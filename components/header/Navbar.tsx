077845e159e452c01fec14fe3a6e0967930e30cc
Stella Soares Santos <54483907+stellassantos@users.noreply.github.com>
2023-07-20 15:50:24 -0300

    Initialize revert-174-lucis/update-text

---
+++ components/header/Navbar.tsx
@@ -0,0 +1,77 @@
+import HeaderSearchMenu from "deco-sites/fashion/islands/HeaderSearchMenu.tsx";
+import HeaderButton from "deco-sites/fashion/islands/HeaderButton.tsx";
+import Icon from "deco-sites/fashion/components/ui/Icon.tsx";
+import NavItem from "./NavItem.tsx";
+import { navbarHeight } from "./constants.ts";
+import type { INavItem } from "./NavItem.tsx";
+import type { Props as SearchbarProps } from "deco-sites/fashion/components/search/Searchbar.tsx";
+
+function Navbar({ items, searchbar }: {
+  items: INavItem[];
+  searchbar: SearchbarProps;
+}) {
+  return (
+    <>
+      {/* Mobile Version */}
+      <div
+        style={{ height: navbarHeight }}
+        class="md:hidden flex flex-row justify-between items-center border-b border-base-200 w-full pl-2 pr-6 gap-2"
+      >
+        <HeaderButton variant="menu" />
+
+        <a
+          href="/"
+          class="flex-grow inline-flex items-center"
+          style={{ minHeight: navbarHeight }}
+          aria-label="Store logo"
+        >
+          <Icon id="Logo" width={126} height={16} />
+        </a>
+
+        <div class="flex gap-1">
+          <HeaderButton variant="search" />
+          <HeaderButton variant="cart" />
+        </div>
+      </div>
+
+      {/* Desktop Version */}
+      <div class="hidden md:flex flex-row justify-between items-center border-b border-base-200 w-full pl-2 pr-6">
+        <div class="flex-none w-44">
+          <a href="/" aria-label="Store logo" class="block px-4 py-3 w-[160px]">
+            <Icon id="Logo" width={126} height={16} />
+          </a>
+        </div>
+        <div class="flex-auto flex justify-center">
+          {items.map((item) => <NavItem item={item} />)}
+        </div>
+        <div class="flex-none w-44 flex items-center justify-end gap-2">
+          <HeaderButton variant="search" />
+          <HeaderSearchMenu searchbar={searchbar} />
+          <a
+            class="btn btn-square btn-ghost"
+            href="/login"
+            aria-label="Log in"
+          >
+            <Icon id="User" width={20} height={20} strokeWidth={0.4} />
+          </a>
+          <a
+            class="btn btn-square btn-ghost"
+            href="/wishlist"
+            aria-label="Wishlist"
+          >
+            <Icon
+              id="Heart"
+              width={20}
+              height={20}
+              strokeWidth={2}
+              fill="none"
+            />
+          </a>
+          <HeaderButton variant="cart" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
